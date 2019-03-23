018f67f6e067a77a9c5326c8fd4e5808e32f22ad
Alex <quacksterr.15@gmail.com>
2019-03-23 17:39:41 +1000

    Copy lingo even if it fails

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace Shockpile
         {
             string node = @"C:\Program Files\nodejs\node.exe";
             string projectraysDirectory = @"C:\Users\Alex\Documents\Decompiler\ProjectorRays";
-            string cstDirectory = @"C:\Users\Alex\Documents\Decompiler\v14";
+            string cstDirectory = @"C:\Users\Alex\Documents\Decompiler\v7";
             string srcDirectory = Path.Combine(cstDirectory, "src");//@"C:\Users\Alex\Documents\Decompiler\v7\src";
 
             if (!Directory.Exists(projectraysDirectory))
@@ -50,22 +50,25 @@ namespace Shockpile
                 if (fileExtension != ".cst")
                     continue;
 
-                string outputDirectory = Path.Combine(projectraysDirectory, fileName);
-
-                if (!Directory.Exists(outputDirectory))
+                if (fileName == "hh_room_pool")
                 {
-                    Process p = new Process();
-                    p.StartInfo.WorkingDirectory = projectraysDirectory;
-                    p.StartInfo.FileName = node;
-                    p.StartInfo.Arguments = "bin/index.js \"" + fullFileName + "\"";
-                    p.Start();
-                    p.WaitForExit();
+                    string outputDirectory = Path.Combine(projectraysDirectory, fileName);
+
+                    if (!Directory.Exists(outputDirectory))
+                    {
+                        Process p = new Process();
+                        p.StartInfo.WorkingDirectory = projectraysDirectory;
+                        p.StartInfo.FileName = node;
+                        p.StartInfo.Arguments = "bin/index.js \"" + fullFileName + "\"";
+                        p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        p.Start();
+                        p.WaitForExit();
+                    }
+
+
+                    PerformCleanup(fullFileName, fileName, outputDirectory, srcDirectory);
                 }
-
-                PerformCleanup(fullFileName, fileName, outputDirectory, srcDirectory);
             }
-
-            Console.Read();
         }
 
         private static void PerformCleanup(string fullFileName, string fileName, string outputDirectory, string srcDirectory)
@@ -83,38 +86,71 @@ namespace Shockpile
                     return;
                 }
 
-                lingoDirectory = Path.Combine(lingoDirectory, Path.GetFileName(directories[0]));
+                string copyDirectory = Path.Combine(lingoDirectory, Path.GetFileName(directories[0]));
 
                 // Move source files
-                string[] sourceFiles = Directory.GetFiles(lingoDirectory);
+                string[] sourceFiles = Directory.GetFiles(copyDirectory);
 
                 foreach (string sourceFile in sourceFiles)
                 {
-                    File.Move(sourceFile, Path.Combine(outputDirectory, Path.GetFileName(sourceFile)));
+                    string fromDirectory = sourceFile;
+                    string toDirectory = Path.Combine(outputDirectory, Path.GetFileName(sourceFile));
+                    File.Move(fromDirectory, toDirectory);
                 }
             }
+
+            bool copyLingoFolder = false;
 
             // Delete useless folders
             try
             {
                 Directory.Delete(Path.Combine(outputDirectory, "casts"), true);
+            } catch { copyLingoFolder = true;  }
+
+            try
+            {
                 Directory.Delete(Path.Combine(outputDirectory, "chunks"), true);
+            }
+            catch { }
+
+            try
+            {
+                if (copyLingoFolder)
+                {
+                    string[] directories = Directory.GetDirectories(Path.Combine(outputDirectory, "lingo"));
+                    string copyDirectory = Path.Combine(outputDirectory, "lingo", Path.GetFileName(directories[0]));
+
+                    // Move source files
+                    string[] sourceFiles = Directory.GetFiles(copyDirectory);
+
+                    foreach (string sourceFile in sourceFiles)
+                    {
+                        string fromDirectory = sourceFile;
+                        string toDirectory = Path.Combine(outputDirectory, Path.GetFileName(sourceFile));
+                        File.Move(fromDirectory, toDirectory);
+                    }
+                }
+
                 Directory.Delete(Path.Combine(outputDirectory, "lingo"), true);
-            } catch { }
+            }
+            catch { }
+
+            string newDirectory = Path.Combine(srcDirectory, "script_" + fileName);
 
             // Move folders
             if (Directory.Exists(outputDirectory))
             {
                 if (!Directory.Exists(outputDirectory))
                 {
-                    Directory.CreateDirectory(Path.Combine(srcDirectory, fileName));
+                    Directory.CreateDirectory(newDirectory);
                 }
 
 
-                Directory.Move(outputDirectory, Path.Combine(srcDirectory, fileName));
+                Directory.Move(outputDirectory, newDirectory);
             }
 
-            Console.WriteLine(lingoDirectory);
+
+            Console.WriteLine("Processed " + fileName);
         }
 
         private static void WriteError(string v, bool block = false)
